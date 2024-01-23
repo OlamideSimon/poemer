01397ff4aa94791f78f5f850bc1c3550a5aab53b
Olamide <simonadepetoye@gmail.com>
2024-01-23 21:45:19 +0100

    poemer

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import StepperForm from './components/StepperForm'
 
 function App() {
   return (
-    <div className="grid place-content-center h-dvh w-dvw bg-bg_image bg-cover bg-no-repeat bg-opacity-5 text-slate-900 p-2">
+    <div className="grid place-content-center min-h-dvh w-dvw bg-bg_image bg-cover bg-no-repeat bg-opacity-5 text-slate-900 p-2">
       <div className="absolute top-0 left-0 w-full h-full bg-white bg-opacity-15"></div>
       <StepperForm />
       {/* Write a love poem for my partner, Alex, that captures the warmth and comfort of our
